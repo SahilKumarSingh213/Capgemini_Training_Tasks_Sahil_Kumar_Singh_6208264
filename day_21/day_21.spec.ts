2ab280b9c83ab76579362f6d29c2cd28bd07ed97
SahilKumarSingh213 <sks.sgrn@gmail.com>
2026-03-17 22:52:36 +0530

    day 21

---
+++ day_21/day_21.spec.ts
@@ -0,0 +1,12 @@
+import { test } from "@playwright/test";
+
+test("kabaddi",async({page})=>{
+
+    await page.goto("https://www.prokabaddi.com/schedule-fixtures-results?tab=recent")
+    // let divs = await page.locator('(//div[@class="fixtures-element"])[1]/div').allTextContents()
+    let match = await page.locator('(//div[@class="fixtures-element"])[1]//div[@class="element element1" or @class="element element2" or @class="element element3"]').allTextContents();
+    for(let i of match){
+        console.log(i);
+        console.log("");
+    }
+})
